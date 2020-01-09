d60084a973ecac099b8bc4ab6ff888d40b35f166
Dainius Matusevicius <dainiauskas@gmail.com>
2020-01-09 05:29:24 +0200

    Changed log path

--- service.go
+++ service.go
@@ -1,18 +1,19 @@
 package service
 
 import (
-  "butent/tools/log"
-  srv "github.com/kardianos/service"
-  "fmt"
+	"fmt"
+
+	"bitbucket.org/butenta/pkg-log"
+	srv "github.com/kardianos/service"
 )
 
 var logger srv.Logger
 
-type Program struct{
-  Name        string
-  DisplayName string
-  Description string
-  CallBack    func()
+type Program struct {
+	Name        string
+	DisplayName string
+	Description string
+	CallBack    func()
 }
 
 func (p *Program) Start(s srv.Service) error {
@@ -23,9 +24,9 @@ func (p *Program) Start(s srv.Service) error {
 func (p *Program) run() error {
 	log.Info("Service running on %v platform.", srv.Platform())
 
-  p.CallBack()
+	p.CallBack()
 
-  return nil
+	return nil
 }
 
 func (p *Program) Stop(s srv.Service) error {
@@ -34,23 +35,23 @@ func (p *Program) Stop(s srv.Service) error {
 }
 
 func New(name, display, desc string) *Program {
-  return &Program{
-    Name: name,
-    DisplayName: display,
-    Description: desc,
-  }
+	return &Program{
+		Name:        name,
+		DisplayName: display,
+		Description: desc,
+	}
 }
 
 func (p *Program) SetCb(cb func()) {
-  p.CallBack = cb
+	p.CallBack = cb
 }
 
 func (p *Program) Controller(param string) error {
-  svcConfig := &srv.Config{
+	svcConfig := &srv.Config{
 		Name:        p.Name,
 		DisplayName: p.DisplayName,
-    Description: p.Description,
-    Arguments: []string{"service"},
+		Description: p.Description,
+		Arguments:   []string{"service"},
 	}
 
 	s, err := srv.New(p, svcConfig)
@@ -63,39 +64,39 @@ func (p *Program) Controller(param string) error {
 		return err
 	}
 
-  var executionError error
-  switch param {
-  case "install":
-    executionError = s.Install()
-    if executionError != nil {
+	var executionError error
+	switch param {
+	case "install":
+		executionError = s.Install()
+		if executionError != nil {
 			return fmt.Errorf("Failed to install: %s", executionError)
 		}
 		fmt.Printf("Service \"%s\" installed.\n", svcConfig.DisplayName)
-  case "uninstall":
+	case "uninstall":
 		executionError = s.Uninstall()
 		if executionError != nil {
 			return fmt.Errorf("Failed to remove: %s", executionError)
 		}
 		fmt.Printf("Service \"%s\" removed.\n", svcConfig.DisplayName)
-  case "stop":
+	case "stop":
 		executionError = s.Stop()
 		if err != nil {
 			return fmt.Errorf("Failed to stop: %s", executionError)
 		}
 		fmt.Printf("Service \"%s\" stopped.\n", svcConfig.DisplayName)
-  case "start":
+	case "start":
 		executionError = s.Start()
 		if executionError != nil {
-      return fmt.Errorf("Failed to start \"%s\" : %s", svcConfig.DisplayName, executionError)
+			return fmt.Errorf("Failed to start \"%s\" : %s", svcConfig.DisplayName, executionError)
 		}
 		fmt.Printf("Service \"%s\" started.\n", svcConfig.DisplayName)
-  default:
+	default:
 		executionError = s.Run()
 		if executionError != nil {
 			return fmt.Errorf("Failed to run \"%s\" : %s", svcConfig.DisplayName, executionError)
 		}
 		fmt.Printf("Service \"%s\" running.\n", svcConfig.DisplayName)
-  }
+	}
 
-  return nil
+	return nil
 }
